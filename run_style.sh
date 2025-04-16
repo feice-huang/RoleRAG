6c0ffa8b2b7bcb6fd04594c241ff0235dca688fb
“BqzzHuang” <bqzz_huang@163.com>
2025-04-16 19:19:37 +0800

    [0416]准备dataset增加一些cot反例，先存个档

--- run_style.sh
+++ run_style.sh
@@ -1,6 +1,6 @@
 # conda activate llama
 cd /data/hfc/RoleRAG
 # 下面CUDA_VISIBLE_DEVICES= 的卡数需要和yaml文件中的ray_num_workers一致
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 USE_RAY=1 llamafactory-cli train /data/hfc/RoleRAG/configs/train_lora/llama3_lora_sft_ray_style.yaml
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 USE_RAY=1 llamafactory-cli train /data/hfc/RoleRAG/configs/train_lora/llama3_lora_sft_ray_style.yaml
 
 llamafactory-cli export configs/merge_lora/llama3_lora_sft_style.yaml
